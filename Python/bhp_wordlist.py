423dc11de23c3af74fe6bc9f384ae85ab40f0a43
Debian <ec2-user@localhost.localdomain>
2019-01-15 13:12:44 +0000

    15_January_2019

--- Python/bhp_wordlist.py
+++ Python/bhp_wordlist.py
@@ -1,15 +1,14 @@
+#ch6_burp_wordlist.py
 from burp import IBurpExtender
 from burp import IContextMenuFactory
-
 from javax.swing import JMenuItem
-from java.util import List, ArrayList
-from java.net import URL
-
-import re
-from datetime import datetime
+from java.util import ArrayList, List
 from HTMLParser import HTMLParser
+from datetime import datetime
+import re
 
-
+#This class attempts to strip all tags from and HTML page recieved in the http response
+#The remaining text will appened to an array and then joined with " " for regex parsing
 class TagStripper(HTMLParser):
     def __init__(self):
         HTMLParser.__init__(self)
@@ -19,94 +18,74 @@ class TagStripper(HTMLParser):
         self.page_text.append(data)
 
     def handle_comment(self, data):
-        self.handle_data(data)
+        self.page_text.append(data)
 
-    def strip(self, html):
-        self.feed(html)
-        return (" ".join(self.page_text))
+    def strip(self, html_page):
+        self.feed(html_page)
+        return " ".join(self.page_text)
 
-
+#BurpExtender Class as per Reference API
 class BurpExtender(IBurpExtender, IContextMenuFactory):
+    #The extension is registered and we initialize the hosts and word list sets
     def registerExtenderCallbacks(self, callbacks):
         self._callbacks = callbacks
         self._helpers = callbacks.getHelpers()
         self.context = None
         self.hosts = set()
-
-        # start with something we know is common
-        self.wordlist = set(["password"])
-
-        # we set up our extension
-        callbacks.setExtensionName("BHP Wordlist")
+        self.word_list = set(["password"])
+        callbacks.setExtensionName("BHP WordList")
         callbacks.registerContextMenuFactory(self)
-
         return
 
+    #Invoke the "Create WordList" Menu
+    def createMenuItems(self, context):
+        self.context = context
+        menu_list = ArrayList()
+        menu_list.add(JMenuItem("Create WordList", actionPerformed=self.menu_action))
+        return menu_list
 
-def createMenuItems(self, context_menu):
-    self.context = context_menu
-    menu_list = ArrayList()
-    menu_list.add(JMenuItem("Create Wordlist", actionPerformed=self.wordlist_menu))
-
-    return menu_list
-
-
-def wordlist_menu(self, event):
-    # grab the details of what the user clicked
-    http_traffic = self.context.getSelectedMessages()
-
-    for traffic in http_traffic:
-        http_service = traffic.getHttpService()
-        host = http_service.getHost()
-
-        self.hosts.add(host)
-
-        http_response = traffic.getResponse()
-
-        if http_response:
-            self.get_words(http_response)
-    self.display_wordlist()
-    return
-
-
-def get_words(self, http_response):
-    headers, body = http_response.tostring().split('\r\n\r\n', 1)
-
-    # skip non-text response
-    if headers.lower().find("content-type: text") == -1:
+    #Menu Action: Obtain the http_host and http_response. The http_host is appened to the hosts set
+    #HTTP Response, if available, is analyzed to create a word list
+    def menu_action(self, event):
+        http_traffic = self.context.getSelectedMessages()
+        for traffic in http_traffic:
+            http_service = traffic.getHttpService()
+            http_host = http_service.getHost()
+            self.hosts.add(http_host)
+            http_response = traffic.getResponse()
+            if http_response:
+                self.get_words(http_response)
+        self.display_words()
         return
 
-    tag_stripper = TagStripper()
-    page_text = tag_stripper.strip(body)
+    #We check the header for a text document in the body. If the content type is text, username
+    #the TagStripper class to parse out the text and run a regex to create our wordlist based on
+    #the regex criteria and word length. The resulting words are added to the word_list set
+    def get_words(self, http_response):
+        headers, body = http_response.tostring().split("\r\n\r\n", 1)
+        if headers.lower().find("content-type: text") == -1:
+            return
+        tag_stripper = TagStripper()
+        page_text = tag_stripper.strip(body)
+        word_candidates = re.findall("[a-zA-Z]\w{2,}", page_text)
+        for word in sorted(word_candidates):
+            if len(word) <= 12:
+                self.word_list.add(word.lower())
+        return
 
-    words = re.findall("[a-zA-Z]\w{2,}", page_text)
+    #This method will display our word list after adding some optional suffices
+    def display_words(self):
+        print "Word List for: %s" % " ".join(self.hosts)
+        for word in self.word_list:
+            self.mangle(word)
+        return
 
-    for word in words:
+    #This method add the suffices below each word in the word_list set.
+    def mangle(self, word):
+        year = datetime.now().year
+        suffices = ["", "!", "1", year]
 
-        # filter out long strings
-        if len(word) <= 12:
-            self.wordlist.add(word.lower())
-
-    return
-
-
-def mangle(self, word):
-    year = datetime.now().year
-    suffixes = ["", "1", "!", year]
-    mangled = []
-
-    for password in (word, word.capitalize()):
-        for suffix in suffixes:
-            mangled.append("%s%s" % (password, suffix))
-
-    return mangled
-
-
-def display_wordlist(self):
-    print("#!comment: BHP Wordlist for site(s) %s" % ", ".join(self.hosts))
-
-    for word in sorted(self.wordlist):
-        for password in self.mangle(word):
-            print(password)
-
-    return
+        for suffix in suffices:
+            print "%s%s" % (word, suffix)
+            print "%s%s" % (word.capitalize(), suffix)
+        return
