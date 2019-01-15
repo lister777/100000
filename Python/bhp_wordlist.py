661faa8aaaeb6905278f58a48f80f39594f33daa
Debian <ec2-user@localhost.localdomain>
2019-01-15 12:08:39 +0000

    14_January_2019

--- Python/bhp_wordlist.py
+++ Python/bhp_wordlist.py
@@ -39,3 +39,70 @@ class BurpExtender(IBurpExtender, IContextMenuFactory):
         callbacks.registerContextMenuFactory(self)
         
         return
+    
+def createMenuItems(self, context_menu):
+    self.context = context_menu
+    menu_list = ArrayList()
+    menu_list.add(JMenuItem("Create Wordlist", actionPerformed=self.wordlist_menu))
+    
+    return menu_list
+    
+def wordlist_menu(self,event):
+    
+    # grab the details of what the user clicked
+    http_traffic = self.context.getSelectedMessages()
+    
+    for traffic in http_traffic:
+        http_service = traffic.getHttpService()
+        host = http_service.getHost()
+        
+        self.hosts.add(host)
+        
+        http_response = traffic.getResponse()
+        
+        if http_response:
+            self.get_words(http_response)
+    self.display_wordlist()
+    return
+
+def get_words(self, http_response):
+    
+    headers, body = http_response.tostring(),split('\r\n\r\n', 1)
+    
+    # skip non-text response
+    if headers.lower().find("content-type: text") == -1:
+        return
+    
+    tag_stripper = TagStripper()
+    page_text = tag_stripper.strip(body)
+    
+    words = re.findall("[a-zA-Z]\w{2,}", page_text)
+    
+    for word in words:
+        
+        # filter out long strings
+        if len(word) <= 12:
+            self.wordlist.add(word.lower())
+    
+    return
+
+def mangle(self, word):
+    year = datetime.now().year
+    suffixes = ["", "1", "!", year]
+    mangled = []
+    
+    for password in (word, word.capitalize()):
+        for suffix in suffixes:
+            mangle.append("%s%s" % (password, suffix))
+    
+    return mangled
+    
+def display_wordlist(self):
+    
+    print("#!comment: BHP Wordlist for site(s) %s" %", ".join(self.hosts))
+    
+    for word in sorted(self.wordlist):
+        for password in self.mangle(word):
+            print(password)
+            
+    return
